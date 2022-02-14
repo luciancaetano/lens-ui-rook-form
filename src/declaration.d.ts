c426f6b3368b53a36043bf8e51e6c25635044671
Lucian Caetano <lucian.santos.caetano@gmail.com>
2022-02-14 20:07:03 -0300

    build(deps): :arrow_up:  Upgrading dependencies

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,13 +1,4 @@
-/* eslint @typescript-eslint/no-empty-interface: 0 */
-import { ThemeTypes } from './types';
-
 declare module '*.scss' {
   const content: Record<string, string>;
   export default content;
-}
-
-declare module 'styled-components' {
-  export interface ILuifDefaultTheme extends ThemeTypes.IThemeSettings{
-    //
-  }
 }
